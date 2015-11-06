48565b3db06a57161f09c790cae60e2be6ac24b9
CuriosityChan <404228340@qq.com>
2015-11-06 20:38:32 +0800

    add task4

---
+++ Task4SimpleCalculator/calculator.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+	var expression = ""
+	var visibleButton = document.querySelectorAll('.visible')
+	var expInput = document.getElementById('expression');
+	
+	for (var i = visibleButton.length - 1; i >= 0; i--) {
+		visibleButton[i].onclick = function(event){
+			expression += event.target.textContent;
+			expInput.value = expression;
+		}
+	}
+
+	document.getElementById('back').onclick = function(event) {
+		if (expInput.value.length > 0) {
+			expression = expression.substring(0, expression.length-1);
+			expInput.value = expression;
+		}
+	}
+
+	document.getElementById('clear').onclick = function(event) {
+		expression = "";
+		expInput.value = "0";
+	}
+
+	document.getElementById('result').onclick = function(event) {
+		try {var result = eval(expression);
+			expInput.value = result;
+			expression = "";
+		} catch (e) {
+			expInput.value = "error!"
+			alert("Input error!")
+			expression= "";
+		}
+	}
+}
